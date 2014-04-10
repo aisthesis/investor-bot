2234f029a569515e58c7d6ed805df65fe996367c
Marshall Farrier <info@marshallfarrier.com>
2014-04-10 15:46:56 -0700

    Modify script to retrieve all available data since 1946. Retrieve data for additional stocks.

--- scripts/yahoo-get.sh
+++ scripts/yahoo-get.sh
@@ -15,4 +15,4 @@
 # Similarly:
 # http://ichart.finance.yahoo.com/table.csv?s=f&a=0&b=1&c=2013&d=5&e=27&f=2013
  
-curl "http://ichart.finance.yahoo.com/table.csv?s=$1&a=0&b=1&c=1974&d=3&e=9&f=2014" > ../data/$1.csv
+curl "http://ichart.finance.yahoo.com/table.csv?s=$1&a=0&b=1&c=1946&d=4&e=1&f=2014" > ../data/$1.csv
