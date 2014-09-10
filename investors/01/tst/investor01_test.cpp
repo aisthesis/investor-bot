71f012404aa4ad573b67633d9db6878ff94c1a39
Marshall Farrier <info@marshallfarrier.com>
2014-09-09 22:48:40 -0700

    Organize unit tests for Investor01

--- investors/01/tst/investor01_test.cpp
+++ investors/01/tst/investor01_test.cpp
@@ -27,84 +27,110 @@
 
 TEST_CASE("correct orders generated from recommendations", "[Investor01]") {
     Investor01 investor;
-    investor.deposit(1000.0);
 
-    // recommend buying foo
-    std::unordered_map<std::string, double> strengths({{"foo", 1.0}});
-    TickerOhlcMap ohlc_map({{"foo", {0.0, 40.0, 1.0, 20.0}}});
-    std::vector<Order> orders = investor.order(strengths, ohlc_map);
+    SECTION("initialization") {
+        REQUIRE(investor.n_long_pos() == 0);
+        REQUIRE(approx(investor.cash(), 0.0));
+        REQUIRE(approx(investor.pending(), 0.0));
+    }
 
-    // single recommendation, empty portfolio, nothing pending
-    REQUIRE(orders.size() == 1);
-    REQUIRE(orders[0].type() == Order::Type::kBuy);
-    REQUIRE(orders[0].mode() == Order::Mode::kLimit);
-    REQUIRE(orders[0].ticker() == "foo");
-    REQUIRE(orders[0].shares() >= 49);
-    REQUIRE(orders[0].share_price() > 19.99);
-    REQUIRE(orders[0].share_price() < 20.01);
-    REQUIRE(investor.pending() > 980.0);
+    SECTION("pending (from superclass)") {
+        double add_to_pending = 100.0;
+        double pending = investor.add_to_pending(add_to_pending);
+        REQUIRE(approx(investor.pending(), add_to_pending));
+        REQUIRE(approx(pending, add_to_pending));
+        investor.clear_pending();
+        REQUIRE(approx(investor.pending(), 0.0));
+    }
 
-    // recommendation with order still pending
-    strengths.clear();
-    ohlc_map.clear();
-    strengths["bar"] = 1.0;
-    ohlc_map["bar"] = {0.0, 100.0, 1.0, 50.0};
-    orders = investor.order(strengths, ohlc_map);
-    // orders empty because other order is still pending
-    REQUIRE(orders.size() == 0);
-    REQUIRE(investor.pending() > 980.0);
+    SECTION("portfolio (from superclass)") {
+        double deposit_amt = 1000.0,
+            expected_cash = 0.0,
+            purchase_price = 500.0,
+            sale_price = 200.0;
+        std::string ticker1 = "blah";
+        int shares_bought = 25,
+            shares_sold = 20;
 
-    // recommendation with no cash
-    // buy foo
-    investor.buy("foo", 50, 1000.0);
-    investor.clear_pending();
-    // foo still a buy
-    strengths["foo"] = 1.0;
-    ohlc_map["foo"] = {0.0, 60.0, 1.0, 30.0};
-    orders = investor.order(strengths, ohlc_map);
-    // here we sell 1/2 of foo to buy bar (no order yet for bar)
-    REQUIRE(orders.size() == 1);
-    REQUIRE(orders[0].type() == Order::Type::kSell);
-    REQUIRE(orders[0].mode() == Order::Mode::kLimit);
-    REQUIRE(orders[0].ticker() == "foo");
-    REQUIRE(orders[0].shares() >= 24);
-    REQUIRE(orders[0].shares() <= 26);
-    REQUIRE(orders[0].share_price() > 29.99);
-    REQUIRE(orders[0].share_price() < 30.01);
-    REQUIRE(investor.pending() < 0.01);
-    REQUIRE(investor.cash() < 0.01);
-    REQUIRE(investor.cash() > -0.01);
+        investor.deposit(deposit_amt);
+        expected_cash += deposit_amt;
+        REQUIRE(approx(investor.cash(), expected_cash));
+        investor.buy(ticker1, shares_bought, purchase_price);
+        expected_cash -= purchase_price;
+        REQUIRE(approx(investor.cash(), expected_cash));
+        REQUIRE(investor.n_long_pos() == 1);
+        REQUIRE(investor.shares(ticker1) == shares_bought);
+        investor.sell(ticker1, shares_sold, sale_price);
+        expected_cash += sale_price;
+        REQUIRE(approx(investor.cash(), expected_cash));
+        REQUIRE(investor.n_long_pos() == 1);
+    }
 
-    // sell half of foo
-    investor.sell("foo", 25, 750.0);
-    orders = investor.order(strengths, ohlc_map);
-    REQUIRE(orders.size() == 1);
-    REQUIRE(orders[0].type() == Order::Type::kBuy);
-    REQUIRE(orders[0].mode() == Order::Mode::kLimit);
-    REQUIRE(orders[0].ticker() == "bar");
-    REQUIRE(orders[0].shares() >= 14);
-    REQUIRE(orders[0].shares() <= 16);
-    REQUIRE(orders[0].share_price() > 49.99);
-    REQUIRE(orders[0].share_price() < 50.01);
-    REQUIRE(investor.pending() > 700.0);
-    REQUIRE(investor.cash() < 750.01);
-    REQUIRE(investor.cash() > 749.99);
+    SECTION("orders") {
+        investor.deposit(1000.01);
 
-    // hold recommendations
-    investor.clear_pending();
-    strengths["foo"] = 0.5;
-    strengths["bar"] = 0.5;
-    orders = investor.order(strengths, ohlc_map);
-    REQUIRE(orders.size() == 0);
+        // recommend buying foo
+        std::unordered_map<std::string, double> strengths({{"foo", 1.0}});
+        TickerOhlcMap ohlc_map({{"foo", {0.0, 40.0, 1.0, 20.0}}});
+        std::vector<Order> orders = investor.order(strengths, ohlc_map);
 
-    // sell recommendation
-    strengths["foo"] = 0.1;
-    orders = investor.order(strengths, ohlc_map);
-    REQUIRE(orders[0].type() == Order::Type::kSell);
-    REQUIRE(orders[0].mode() == Order::Mode::kLimit);
-    REQUIRE(orders[0].ticker() == "foo");
-    REQUIRE(orders[0].shares() == 25);
-    REQUIRE(orders[0].share_price() > 29.99);
-    REQUIRE(orders[0].share_price() < 30.01);
-    REQUIRE(investor.pending() < 0.01);
+        SECTION("single recommendation, empty portfolio, nothing pending") {
+            REQUIRE(orders.size() == 1);
+            REQUIRE(orders[0] == Order(Order::Type::kBuy, Order::Mode::kLimit, "foo", 50, 20.0));
+            REQUIRE(approx(investor.pending(), 1000.0));
+        }
+
+        // recommendation with order still pending but nothing to sell
+        strengths.clear();
+        ohlc_map.clear();
+        strengths["bar"] = 1.0;
+        ohlc_map["bar"] = {0.0, 100.0, 1.0, 50.0};
+        orders = investor.order(strengths, ohlc_map);
+        SECTION("orders empty because other order is still pending") {
+            REQUIRE(orders.size() == 0);
+            REQUIRE(approx(investor.pending(), 1000.0));
+        }
+
+        // buy foo to use up all but 0.01 of cash
+        investor.buy("foo", 50, 1000.0);
+        investor.clear_pending();
+        // foo still a buy, bar also a buy (cf. above)
+        strengths["foo"] = 1.0;
+        ohlc_map["foo"] = {0.0, 60.0, 1.0, 30.0};
+        orders = investor.order(strengths, ohlc_map);
+        // here we should sell 1/2 of foo to buy bar (no order yet for bar)
+        SECTION("orders with nothing pending but insufficient cash") {
+            REQUIRE(orders.size() == 1);
+            REQUIRE(orders[0] == Order(Order::Type::kSell, Order::Mode::kLimit, "foo", 25, 30.0));
+            REQUIRE(approx(investor.pending(), 0.0));
+            REQUIRE(approx(investor.cash(), 0.01));
+        }
+
+        // sell half of foo, total cash will now be 750.01, nothing pending
+        investor.sell("foo", 25, 750.0);
+        orders = investor.order(strengths, ohlc_map);
+        SECTION("order with enough cash and 1 stock in portfolio") {
+            REQUIRE(orders.size() == 1);
+            REQUIRE(orders[0] == Order(Order::Type::kBuy, Order::Mode::kLimit, "bar", 15, 50.0));
+            REQUIRE(approx(investor.pending(), 750.0));
+            REQUIRE(approx(investor.cash(), 750.01));
+        }
+
+        // hold recommendations
+        investor.clear_pending();
+        strengths["foo"] = 0.5;
+        strengths["bar"] = 0.5;
+        orders = investor.order(strengths, ohlc_map);
+        SECTION("hold recommendations") {
+            REQUIRE(orders.size() == 0);
+        }
+
+        // sell recommendation
+        strengths["foo"] = 0.1;
+        orders = investor.order(strengths, ohlc_map);
+        SECTION("sell recommendation") {
+            REQUIRE(orders[0] == Order(Order::Type::kSell, Order::Mode::kLimit, "foo", 25, 30.0));
+            REQUIRE(approx(investor.pending(), 0.0));
+        }
+    }
 }
