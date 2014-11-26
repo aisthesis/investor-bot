ffa43a8251623c8ea1bb5b486869dde0132958e2
Marshall Farrier <info@marshallfarrier.com>
2014-11-26 11:34:51 -0800

    Verify in build script that necessary Python packages are installed.

---
+++ lib/tst/pytst.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import imp
+
+dependencies = ('matplotlib', 'numpy', 'pandas', 'pynance')
+
+def test_pkg(pkg):
+    try:
+        imp.find_module(pkg)
+        print("OK. Package '{0}' found.".format(pkg))
+    except ImportError:
+        print("\033[91mPackage '{0}' not found. Please install '{0}' on your system.\033[0m".format(pkg))
+
+print("Python 2.x dependencies:")
+
+for pkg in dependencies:
+    test_pkg(pkg)
+
+print("Note: Python 2.x dependencies are not needed if you use only Python 3.x\n")
