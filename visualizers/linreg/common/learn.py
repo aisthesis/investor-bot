46724c27325fdeae27f624c871cb579aaf4a9a76
Marshall Farrier <info@marshallfarrier.com>
2015-11-10 22:13:33 -0800

    Get learning parameters.

---
+++ visualizers/linreg/common/learn.py
@@ -0,0 +1,38 @@
+"""
+Apply linear regression to data.
+
+Copyright (c) 2015 Marshall Farrier
+
+Distribution and use of this software without prior written permission
+of the authors is strictly prohibitied and will be prosecuted to
+the full extent of the law.
+
+by
+
+Marshall Farrier, marshalldfarrier@gmail.com
+"""
+
+import numpy as np
+import pynance as pn
+
+def preprocess(features, **kwargs):
+    has_means = bool('means' in kwargs)
+    has_sd_adj = bool('sd_adj' in kwargs)
+    if has_means ^ has_sd_adj:
+        raise ValueError('both or neither of means and sd_adj required')
+    if has_means and has_sd_adj:
+        means = kwargs['means']
+        sd_adj = kwargs['sd_adj']
+        features[:, 1:] -= means
+        features[:, 1:] *= 1. / sd_adj
+        return means, sd_adj
+    _, means = pn.data.center(features[:, 1:], out=features[:, 1:])
+    _, sd_adj = pn.data.normalize(features[:, 1:], out=features[:, 1:])
+    return means, sd_adj
+
+def get_model(features, labels):
+    means, sd_adj = preprocess(features)
+    model = np.linalg.lstsq(features.transpose().dot(features),
+            features.transpose().dot(labels))[0]
+    return model, means, sd_adj
+
