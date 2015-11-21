66bab757af9b83615b34ccdd1ba5f7fb4b6174b7
Marshall Farrier <info@marshallfarrier.com>
2015-11-21 09:42:48 -0800

    Stub visualize module.

---
+++ visualizers/linreg/common/visualize.py
@@ -0,0 +1,19 @@
+"""
+Visualize equity data
+
+Copyright (c) 2015 Marshall Farrier
+
+Distribution and use of this software without prior written permission
+of the authors is strictly prohibitied and will be prosecuted to
+the full extent of the law.
+
+by
+
+Marshall Farrier, marshalldfarrier@gmail.com
+"""
+
+def actvpred(eqdata, modelfile):
+    pass
+
+def predgrowth(eqdata, modelfile):
+    pass
