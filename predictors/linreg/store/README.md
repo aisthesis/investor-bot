699012d818b4bc6906b7dcf6f24d25481876e6f9
Marshall Farrier <info@marshallfarrier.com>
2015-12-03 18:44:22 -0800

    Virtual environment including pymongo.

--- predictors/linreg/store/README.md
+++ predictors/linreg/store/README.md
@@ -2,3 +2,12 @@ predictors/linreg/store
 ===
 Code for storing linear regression results to be used
 in backtesting.
+
+Requirements
+===
+Use `requirements.txt` to install the necessary packages
+in your virtual environment:
+
+    mkvirtualenv invbot
+    workon invbot
+    pip install -r requirements.txt
---
+++ predictors/linreg/store/save_preds.py
@@ -0,0 +1,22 @@
+"""
+predictors/linreg/store/save_preds.py
+
+Save predictions to mongo
+
+Copyright (c) 2015 Marshall Farrier
+
+Distribution and use of this software without prior written permission
+of the authors is strictly prohibitied and will be prosecuted to
+the full extent of the law.
+
+by
+
+Marshall Farrier, marshalldfarrier@gmail.com
+"""
+
+import constants
+import pred_data
+
+def insert(predictions):
+    pass
+    
