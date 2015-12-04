484e5a45772f673146933dd23687fa0079e618c1
Marshall Farrier <info@marshallfarrier.com>
2015-12-03 22:45:53 -0800

    Save out of sample predictions to mongo.

--- predictors/linreg/store/save_preds.py
+++ predictors/linreg/store/save_preds.py
@@ -14,9 +14,14 @@ by
 Marshall Farrier, marshalldfarrier@gmail.com
 """
 
-import pymongo
+import os
+import traceback
+
+from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
 
 import constants
+import pred_data
 
 def _tolist(equity, predictions):
     colnames = predictions.columns.tolist()
@@ -25,7 +30,7 @@ def _tolist(equity, predictions):
 
 def _todict(equity, rowtimestamp, row, colnames):
     datekeys = ('Year', 'Month', 'Day')
-    ret = {'Equity': equity}
+    ret = {'Equity': equity.upper()}
     ret['Year'] = rowtimestamp.year
     ret['Month'] = rowtimestamp.month
     ret['Day'] = rowtimestamp.day
@@ -33,6 +38,46 @@ def _todict(equity, rowtimestamp, row, colnames):
         ret[colname] = row.loc[colname]
     return ret
 
-def insert(equity, predictions):
-    return _tolist(equity, predictions)
+def _get_equities():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    proj_root = os.path.normpath(os.path.join(cwd, '../../..'))
+    path = os.path.join(proj_root, constants.DATA_PATH)
+    eqfile = os.path.join(path, 'oos02.csv')
+    return [line.strip() for line in open(eqfile)]
+
+def insert(bulk, equity, predictions):
+    entries = _tolist(equity, predictions)
+    for entry in entries:
+        bulk.insert(entry)
+    try:
+        result = bulk.execute()
+    except BulkWriteError as e:
+        print("error writing predictions for '{}'".format(equity))
+        print("{} records inserted for '{}'".format(e.details['nInserted'], equity))
+    else:
+        print("{} records inserted for '{}'".format(result['nInserted'], equity))
     
+def run():
+    equities = _get_equities()
+    client = MongoClient()
+    print('db connection opened')
+    db = client[constants.DB_NAME]
+    coll = db[constants.DB_COLLECTION]
+    try:
+        for equity in equities:
+            if coll.find_one({'Equity': equity}):
+                print("'{}' already present and will not be inserted".format(equity))
+                continue
+            print("getting predictions for '{}'".format(equity))
+            try:
+                predictions = pred_data.get(equity, constants.START, constants.END)
+            except (IOError, ValueError):
+                traceback.print_exc()
+                print("no predictions to insert for '{}'".format(equity))
+                continue
+            print("{} predictions retrieved for '{}'".format(predictions.shape[0], equity))
+            bulk = coll.initialize_unordered_bulk_op()
+            insert(bulk, equity, predictions)
+    finally:
+        client.close()
+        print('db connection closed')
